6804d3508c1a2e31207f91855d70c606013bd943
José Isidro <isidro107109@gmail.com>
2025-10-29 05:55:16 +0000

    estructura hecha

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,21 +6,33 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<div className="container bg-black ">
+			<div className="row p-4  text-center mx-3">
+				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
+					<h1 className="m-5 ">
+						<i className="fa-regular fa-clock"></i>
+					</h1>
+				</div>
+				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
+					<h1 className="m-5">0</h1>
+				</div>
+				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
+					<h1 className="m-5 ">0</h1>
+				</div>
+				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
+					<h1 className="m-5 ">0</h1>
+				</div>
+				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
+					<h1 className="m-5 ">0</h1>
+				</div>
+				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
+					<h1 className="m-5 ">0</h1>
+				</div>
+				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
+					<h1 className="m-5">0</h1>
+				</div>
+			</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
 	);
 };
