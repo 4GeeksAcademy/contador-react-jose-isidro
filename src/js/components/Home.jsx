47239b70d0fe4d919562dbd19dd934801b04277c
José Isidro <isidro107109@gmail.com>
2025-10-31 19:08:59 +0000

    proyecto terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,8 +3,14 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+
+const Home = ({seg}) => {
+let segundos1 = seg % 10;
+  let segundos2 = Math.floor(seg / 10) % 6;
+  let minutos1 = Math.floor(seg / 60) % 10;
+  let minutos2 = Math.floor(seg / 600) % 6;
+  let horas1 = Math.floor(seg / 3600) % 10;
+  let horas2 = Math.floor(seg / 36000);
 	return (
 		<div className="container bg-black ">
 			<div className="row p-4  text-center mx-3">
@@ -14,25 +20,24 @@ const Home = () => {
 					</h1>
 				</div>
 				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
-					<h1 className="m-5">0</h1>
+					<h1 className="m-5" >{horas2}</h1>
 				</div>
 				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
-					<h1 className="m-5 ">0</h1>
+					<h1 className="m-5" >{horas1}</h1>
 				</div>
 				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
-					<h1 className="m-5 ">0</h1>
+					<h1 className="m-5" >{minutos2}</h1>
 				</div>
 				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
-					<h1 className="m-5 ">0</h1>
+					<h1 className="m-5" >{minutos1}</h1>
 				</div>
 				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
-					<h1 className="m-5 ">0</h1>
+					<h1 className="m-5" >{segundos2}</h1>
 				</div>
 				<div className="col border border-secondary-subtle text-light rounded-2 py-5 mx-2 bg-dark">
-					<h1 className="m-5">0</h1>
+					<h1 className="m-5"> {segundos1}</h1>
 				</div>
 			</div>
-
 		</div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,20 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
 
+import Home from './components/Home';
+let segundos1 = 0;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Home seg={segundos1} />
   </React.StrictMode>,
 )
+
+//create your first component
+setInterval(() =>{
+  segundos1++;
+  ReactDOM.createRoot(document.getElementById('root')).render(
+      <Home seg={segundos1} />
+  )
+}, 1000)
+
